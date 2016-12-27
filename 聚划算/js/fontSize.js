b93be5a22f949866f6fe1ace07b2e39a9e608613
yangyuliang <s844510534@163.com>
2016-12-27 20:08:53 +0800

    提交1

--- 聚划算/js/fontSize.js
+++
@@ -1,7 +0,0 @@
-(function(win,doc){
-			change();
-			 function change(){
-			 	doc.documentElement.style.fontSize = doc.documentElement.clientWidth*20/320+'px';
-			 }
-			 win.addEventListener('resize',change,false)
-		})(window,document);
